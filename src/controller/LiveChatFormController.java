2f97ebf4c99440ae2722042e275e71ff0e782d3c
Irangaumayantha2001 <iamiranga9@gmail.com>
2022-08-14 15:11:46 +0530

    add livechat  form and livechatform controller

---
+++ src/controller/LiveChatFormController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class LiveChatFormController {
+    public AnchorPane context;
+    public TextArea txtAreaAllText;
+    public TextField txtMessage;
+    public Label lblname;
+
+    public void btnSendOnAction(MouseEvent mouseEvent) {
+    }
+}
