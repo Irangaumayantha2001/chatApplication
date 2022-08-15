e2ae2f71576364120a43f12152ae63b3230650ec
Irangaumayantha2001 <iamiranga9@gmail.com>
2022-08-16 00:33:59 +0530

    update titel

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -25,7 +25,7 @@ public class LoginFormController {
         Stage stage2=new Stage();
         stage2.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ServerForm.fxml"))));
         stage2.setResizable(false);
-        stage2.setTitle("sample title");
+        stage2.setTitle("Live-Chat");
         stage2.centerOnScreen();
         stage2.show();
     }
